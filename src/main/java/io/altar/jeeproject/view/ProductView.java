aabccd78ac9abcc2bb1cfe8d4a660552b42acbe5
DuarteMF <duartepferreira@hotmail.com>
2017-06-21 11:35:36 +0100

    added column reorder, removed stick header, and corrected the view option

--- src/main/java/io/altar/jeeproject/view/ProductView.java
+++ src/main/java/io/altar/jeeproject/view/ProductView.java
@@ -4,9 +4,13 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.ReorderEvent;
 
 import io.altar.jeeproject.model.Product;
 import io.altar.jeeproject.service.ProductService;
@@ -45,5 +49,10 @@ public class ProductView implements Serializable {
  
     public void setSelectedProduct(Product selectedProduct) {
         this.selectedProduct = selectedProduct;
+    }
+    
+    public void onRowReorder(ReorderEvent event) {
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Row Moved", "From: " + event.getFromIndex() + ", To:" + event.getToIndex());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 }
