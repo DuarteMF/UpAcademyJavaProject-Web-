8c48ef7f0b3a2cf9caa276565d92c4b760309149
DuarteMF <duartepferreira@hotmail.com>
2017-06-25 17:26:55 +0100

    starting on having user input the product parameters (not working yet)

--- src/main/java/io/altar/jeeproject/view/ProductView.java
+++ src/main/java/io/altar/jeeproject/view/ProductView.java
@@ -11,61 +11,109 @@ import javax.inject.Inject;
 import io.altar.jeeproject.model.Product;
 import io.altar.jeeproject.service.ProductService;
 
-@ManagedBean(name="ProductView")
+@ManagedBean(name = "ProductView")
 @RequestScoped
 public class ProductView implements Serializable {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-//	private List<Product> products;
+	// private List<Product> products;
 	private Product selectedProduct;
-	
-	@Inject static ProductService productService;
- 
-//    @PostConstruct
-//    public void init() {
-//    	products = productService.showProducts();
-//    }
-	
-//	private static final List<Product> products = productService.showProducts();
+
+	@Inject
+	static ProductService productService;
+
+	private String productName = "";
+	private ArrayList<Integer> productShelfIdLocation;
+	private Integer productDiscount;
+	private Integer productTax;
+	private Double productPrice;
+
+	// @PostConstruct
+	// public void init() {
+	// products = productService.showProducts();
+	// }
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public ArrayList<Integer> getProductShelfIdLocation() {
+		return productShelfIdLocation;
+	}
+
+	public void setProductShelfIdLocation(ArrayList<Integer> productShelfIdLocation) {
+		this.productShelfIdLocation = productShelfIdLocation;
+	}
+
+	public Integer getProductDiscount() {
+		return productDiscount;
+	}
+
+	public void setProductDiscount(Integer productDiscount) {
+		this.productDiscount = productDiscount;
+	}
+
+	public Integer getProductTax() {
+		return productTax;
+	}
+
+	public void setProductTax(Integer productTax) {
+		this.productTax = productTax;
+	}
+
+	public Double getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(Double productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	// private static final List<Product> products =
+	// productService.showProducts();
 	private static final List<Product> products = new ArrayList<>();
-     
-    public List<Product> getProducts() {
-        return products;
-    }
-    
-    public ProductService getProductService() {
-        return productService;
-    }
- 
-    public void setProductService(ProductService productService) {
-//        this.productService = productService;
-        ProductView.productService = productService;
-    }
-    
-    public Product getSelectedProduct() {
-        return selectedProduct;
-    }
- 
-    public void setSelectedProduct(Product selectedProduct) {
-        this.selectedProduct = selectedProduct;
-    }
-    
-    public String addProduct(){
-    	Product product = new Product(null ,"teste",1,6,5.0);
-//    	productService.addProduct(product);
-    	products.add(product);
-    	return null;
-    }
-    
-    public String editProduct(){
-    	return null;
-    }
-    
-    public String deleteProduct(){
-    	products.remove(selectedProduct);
-    	return null;
-    }
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public ProductService getProductService() {
+		return productService;
+	}
+
+	public void setProductService(ProductService productService) {
+		// this.productService = productService;
+		ProductView.productService = productService;
+	}
+
+	public Product getSelectedProduct() {
+		return selectedProduct;
+	}
+
+	public void setSelectedProduct(Product selectedProduct) {
+		this.selectedProduct = selectedProduct;
+	}
+
+	public String addProduct() {
+		// productService.addProduct(product);
+		products.add(new Product(this.productShelfIdLocation, this.productName, this.productDiscount, this.productTax,
+				this.productPrice));
+		return null;
+	}
+
+	public String editProduct() {
+		return null;
+	}
+
+	public String deleteProduct() {
+		products.remove(selectedProduct);
+		return null;
+	}
 }
